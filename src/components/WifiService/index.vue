593dbfdf4525afebeb95ebed044194cd2c62602d
longdt <longdt@vccloud.vn>
2018-12-06 13:15:31 +0700

    .

---
+++ src/components/WifiService/index.vue
@@ -0,0 +1,199 @@
+<template>
+<section>
+<section class="about-banner relative">
+  <div class="overlay overlay-bg"></div>
+  <div class="container">
+    <div class="row d-flex align-items-center justify-content-center">
+      <div class="about-content col-lg-12">
+        <h1 class="text-white">
+          Wifi Quốc Tế
+        </h1>
+        <!-- <p class="text-white link-nav"><a href="index.html">Home </a> <span class="lnr lnr-arrow-right"></span> <a href="hotels.html"> Hotels</a></p> -->
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Start destinations Area -->
+<section class="destinations-area section-gap">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="menu-content pb-40 col-lg-8">
+        <div class="title text-center">
+          <h1 class="mb-10">Dịch vụ sản phẩm</h1>
+          <p>Chúng tôi sẽ giao hàng cho bạn trước 1 ngày khi du lịch và nhận lại sau 1 ngày khi bạn về</p>
+          <p style="color: red">Lưu ý: Nếu trả hàng quá hạn 2 ngày sẽ bị tính thêm phí dịch vụ</p>
+          <p style="color: red">Các thiết bị đều phải đặt cọc nhằm đảm bảo việc khách hàng giữ gìn sản phẩm</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="single-destinations">
+          <div class="thumb">
+            <img src="/static/img/hotels/d1.jpg" alt="">
+          </div>
+          <div class="details">
+            <h4 class="d-flex justify-content-between">
+              <span>Philipin</span>
+              <div class="star">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+              </div>
+            </h4>
+            <p>
+              View on map | 49 Reviews
+            </p>
+            <ul class="package-list">
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Swimming pool</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Gymnesium</span>
+                <span>No</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Wi-fi</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Room Service</span>
+                <span>No</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Air Condition</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Restaurant</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Price per night</span>
+                <a href="#" class="price-btn">$250</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-destinations">
+          <div class="thumb">
+            <img src="/static/img/hotels/d2.jpg" alt="">
+          </div>
+          <div class="details">
+            <h4 class="d-flex justify-content-between">
+              <span>Hàn Quốc</span>
+              <div class="star">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+              </div>
+            </h4>
+            <p>
+              View on map | 49 Reviews
+            </p>
+            <ul class="package-list">
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Swimming pool</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Gymnesium</span>
+                <span>No</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Wi-fi</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Room Service</span>
+                <span>No</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Air Condition</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Restaurant</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Price per night</span>
+                <a href="#" class="price-btn">$250</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-destinations">
+          <div class="thumb">
+            <img src="/static/img/hotels/d3.jpg" alt="">
+          </div>
+          <div class="details">
+            <h4 class="d-flex justify-content-between">
+              <span>Trung Quốc</span>
+              <div class="star">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+              </div>
+            </h4>
+            <p>
+              View on map | 49 Reviews
+            </p>
+            <ul class="package-list">
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Swimming pool</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Gymnesium</span>
+                <span>No</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Wi-fi</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Room Service</span>
+                <span>No</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Air Condition</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Restaurant</span>
+                <span>Yes</span>
+              </li>
+              <li class="d-flex justify-content-between align-items-center">
+                <span>Price per night</span>
+                <a href="#" class="price-btn">$250</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End destinations Area -->
+</section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Layout from '@/components/Layout'
+import Homepage from '@/components/Homepage'
+import WifiService from '@/components/WifiService'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout,
+      redirect: '/trang-chu',
+      children: [
+        {
+          path: 'trang-chu',
+          name: 'Homepage',
+          component: Homepage
+        },
+        {
+          path: 'wifi-quoc-te',
+          name: 'WifiService',
+          component: WifiService
+        }
+      ]
+    }
+  ]
+})
